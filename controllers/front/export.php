e008dc05d291f86c44fef3f36faa16af0ffa50e7
quarkcomro <github@quark.com.ro>
2026-05-19 19:18:19 +0300

    Secure export by adding shop ID filter

--- controllers/front/export.php
+++ controllers/front/export.php
@@ -1,7 +1,8 @@
 <?php
-if (!defined('_PS_VERSION_')) {
-    exit;
-}
+/**
+ * AdWord Engine TXT Excluder API Link
+ * Secures exports per precise Context Shop ID queries.
+ */
 
 class AdvClickFraudExportModuleFrontController extends ModuleFrontController
 {
@@ -10,24 +11,22 @@ class AdvClickFraudExportModuleFrontController extends ModuleFrontController
         $this->ajax = true;
         parent::initContent();
 
-        // Cheie de securitate simplă pentru a preveni scanarea publică a IP-urilor blocate
         $secureToken = md5($this->module->name . _COOKIE_KEY_);
         $providedToken = Tools::getValue('secure_key');
+        $id_shop = (int)Tools::getValue('id_shop', $this->context->shop->id);
 
         if ($secureToken !== $providedToken) {
             header('HTTP/1.1 403 Forbidden');
-            exit('Acces interzis. Token invalid.');
+            exit('Access Denied');
         }
 
-        // Preluăm doar IP-urile care au atins pragul de Fraudă Critică (scor >= 70)
         $ips = Db::getInstance()->executeS('
             SELECT DISTINCT `ip_address` 
             FROM `' . _DB_PREFIX_ . 'adv_click_fraud_logs` 
-            WHERE `fraud_score` >= 70
+            WHERE `fraud_score` >= 70 AND `id_shop` = ' . (int)$id_shop
         ');
 
         header('Content-Type: text/plain; charset=utf-8');
-        
         if (!empty($ips)) {
             foreach ($ips as $ip) {
                 echo $ip['ip_address'] . "\n";
